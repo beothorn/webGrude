b7da80ecf74b964b6b9b4155d7ef4bcd3fb28c92
Lucas <lucass@gmail.com>
2014-02-14 00:45:59 -0200

    Instantiating correctly

--- src/main/java/webToJava/Browser.java
+++ src/main/java/webToJava/Browser.java
@@ -5,16 +5,16 @@ import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import webToJava.annotations.PageURL;
 import webToJava.annotations.Selector;
-import webToJava.elements.Link;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.DomNode;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class Browser {
 	
@@ -28,6 +28,50 @@ public class Browser {
 		}
 	}
 
+	private static List populate(final DomNode node, final Class classs) throws InstantiationException, IllegalAccessException{
+		final ArrayList newInstanceList = new ArrayList();
+		newInstanceList.add(classs.newInstance());
+		return newInstanceList;
+	}
+	
+	private static <T> T loadDomContents(final DomNode node, final Class<T> classs) throws InstantiationException, IllegalAccessException{
+		final T newInstance = classs.newInstance();
+		
+		if(classs.getAnnotation(Selector.class) == null && classs.getAnnotation(PageURL.class) == null)
+			return newInstance;
+		
+		final Field[] declaredFields = classs.getDeclaredFields();
+		for (final Field f : declaredFields) {
+			final Class<?> fieldClass = f.getType();
+			
+			if(fieldClass.equals(java.util.List.class)){
+				final Type genericType = f.getGenericType();
+				final Class<?> listClass = (Class<?>) ((ParameterizedType)genericType).getActualTypeArguments()[0];
+				
+				if(listClass.getAnnotation(Selector.class) != null){
+					f.set(newInstance, populate(node, listClass));
+				}
+			}
+			
+			if(f.getAnnotation(Selector.class) != null){
+				if(fieldClass.equals(java.util.List.class)){
+					final Type genericType = f.getGenericType();
+					final Class<?> listClass = (Class<?>) ((ParameterizedType)genericType).getActualTypeArguments()[0];
+					
+					f.set(newInstance, populate(node, listClass));
+				}else{				
+					f.set(newInstance, fieldClass.newInstance());
+				}
+			}
+			
+			if(fieldClass.getAnnotation(Selector.class) != null){							
+				f.set(newInstance, loadDomContents(node, fieldClass));
+			}
+			
+		}
+		return newInstance;
+	}
+	
 	private static <T> T internalLoad(final Class<T> annotatedClass)
 			throws FailingHttpStatusCodeException, MalformedURLException,
 			IOException, InstantiationException, IllegalAccessException {
@@ -35,9 +79,11 @@ public class Browser {
 		if (!annotatedClass.isAnnotationPresent(PageURL.class)) throw new RuntimeException("Ops, you forgot to add the @"+PageURL.class.getSimpleName());
 		
 		final String pageUrl = annotatedClass.getAnnotation(PageURL.class).value();
-		final HtmlPage page = webClient.getPage(pageUrl);
+		final DomNode page = null;//webClient.getPage(pageUrl);		
 		
+		return loadDomContents(page, annotatedClass);
 		
+		/*
 		final Field[] declaredFields = annotatedClass.getDeclaredFields();
 		final T newInstance = annotatedClass.newInstance();
 		for (final Field f : declaredFields) {
@@ -58,9 +104,10 @@ public class Browser {
 			}
 		}
 		return newInstance;
+		*/
 	}
-
-	private static <T> T load(final Class<T> type, final HtmlPage page) throws InstantiationException, IllegalAccessException {
+	/*
+	private static <T> T load(final Class<T> type, final DomNode page) throws InstantiationException, IllegalAccessException {
 		if (!type.isAnnotationPresent(Selector.class)) return null;
 		final String selector = type.getAnnotation(Selector.class).value();
 		final DomNode querySelector = page.querySelector(selector);
@@ -86,5 +133,5 @@ public class Browser {
 		
 		return null;
 	}
-
+*/
 }
