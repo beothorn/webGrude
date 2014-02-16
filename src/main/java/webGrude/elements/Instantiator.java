0557f6493b7df1266bcafbd134e337a0617fe82a
Lucas <lucass@gmail.com>
2014-02-15 22:07:57 -0200

    Adding more type to Instantiator

--- src/main/java/webGrude/elements/Instantiator.java
+++ src/main/java/webGrude/elements/Instantiator.java
@@ -3,8 +3,6 @@ package webGrude.elements;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.text.html.HTML;
-
 import org.jsoup.nodes.Element;
 
 @SuppressWarnings("rawtypes")
@@ -14,7 +12,8 @@ public class Instantiator {
 	static {
 		classes = new ArrayList<Class>();
 		classes.add(String.class);
-		classes.add(HTML.class);
+		classes.add(Link.class);
+		classes.add(Element.class);
 	}
 	
 	public static boolean typeIsKnown(final Class c){
@@ -25,6 +24,8 @@ public class Instantiator {
 	public static <T> T instanceForNode(final Element node, final Class<T> c){
 		if(c.equals(Element.class))
 			return (T) node;
+		if(c.equals(Link.class))
+			return (T) new Link<T>();
 		return (T) node.text();
 	}
 }
