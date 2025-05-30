1e149ca651bdcac403097702d7c295e8c5a5af82
Beothorn <lucass@gmail.com>
2025-05-30 22:52:52 +0200

    Fix javadocs

--- src/main/java/webGrude/mapping/annotations/Page.java
+++ src/main/java/webGrude/mapping/annotations/Page.java
@@ -8,46 +8,58 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotates a class to be mapped from html.
+ * Annotates a class to be mapped from HTML.
  * <p>
- * The value is the url that wil be prepended to links.
- * The Value is optional.<br>
- * It's also possible to parameterize the url using the tokens {0}, {1} ...
+ * The value is the URL that will be prepended to links. The value is optional.<br>
+ * It's also possible to parameterize the URL using the tokens {0}, {1}, etc.
  * </p>
- * <p>
- * A class to map, the example home page would be annotated as such <br>
- * </p>
- * <pre>
- * {@code @Page
- * class Foo}
- * </pre>
- * <pre>
- * <pre>
- * {@code
- *  Webgrude webgrude = new Webgrude();
- *  Foo foo = webgrude.map(contents, Foo.class, url);}
- * </pre>
+ *
+ * <p>Example usage:</p>
+ *
+ * <pre>{@code
+ * @Page
+ * class Foo {
+ *     // ...
+ * }
+ * }</pre>
+ *
+ * <pre>{@code
+ * Webgrude webgrude = new Webgrude();
+ * Foo foo = webgrude.map(contents, Foo.class, url);
+ * }</pre>
+ *
  * <p>Or:</p>
- * {@code @Page("http://www.example.com")}
- * </pre>
- * <p>
- * A class to load a url parameterized could be like this <br>
- * </p>
- * <pre>
- * {@code @Page("http://www.example.com/{0}/foo={1}/bar")
- * class Foo}
- * </pre>
- * <pre>
- * {@code OkHttpBrowser.get(Foo.class, "toReplace0", "toReplace1");}
- * </pre>
- * The url can be empty and defined on the Browser open call<br>
- * <pre>
- * {@code @Page
- * class Foo}
- * </pre>
- * <pre>
- * {@code OkHttpBrowser.get("http://www.example.com", Foo.class);}
- * </pre>
+ *
+ * <pre>{@code
+ * @Page("http://www.example.com")
+ * class Foo {
+ *     // ...
+ * }
+ * }</pre>
+ *
+ * <p>With URL parameters:</p>
+ *
+ * <pre>{@code
+ * @Page("http://www.example.com/{0}/foo={1}/bar")
+ * class Foo {
+ *     // ...
+ * }
+ * }</pre>
+ *
+ * <pre>{@code
+ * OkHttpBrowser.get(Foo.class, "toReplace0", "toReplace1");
+ * }</pre>
+ *
+ * <p>You can also define the URL during the browser open call:</p>
+ *
+ * <pre>{@code
+ * @Page
+ * class Foo {
+ *     // ...
+ * }
+ *
+ * OkHttpBrowser.get("http://www.example.com", Foo.class);
+ * }</pre>
  *
  * @author beothorn
  * @see OkHttpBrowser#get(Class, String...)
@@ -55,5 +67,9 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Page {
+    /**
+     * The url to visit and prepend to links.
+     * @return the url
+     */
     String value() default "";
 }
